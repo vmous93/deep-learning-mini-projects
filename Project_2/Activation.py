df6a4f41fa3afc405ff3bbbce16703790200763e
Seyedvahid Mousavinezhad <seyedvahid.mousavinezhad@etu.unige.ch>
2022-12-08 09:27:44 +0000

    Update Activation.py (add ReLU and LeakyReLU)

--- Project_2/Activation.py
+++ Project_2/Activation.py
@@ -37,9 +37,42 @@ class Tanh(Module):
 
     def backward(self, gradwrtoutput):
         # 1 - Tanh(x)**2 * gradwrtoutput
-        return (1 - (self.forward(self.value) ** 2)) * gradwrtoutput 
+        return (1 - (self.forward(self.value) ** 2)) * gradwrtoutput
 
 
 
+class ReLU(Module):
+    def __init__(self):
+        super().__init__()
+        self.value = 0
+        self.name = 'ReLU'
+
+    def forward(self, input):
+        # ReLU(x) = max{0,x}
+        self.value = input
+        self.value[self.value <= 0] = 0 
+        return self.value
+
+    def backward(self, gradwrtoutput):
+        # [x > 0 = 1, x < 0 = 0] * gradwrtoutput
+        self.value = self.value > 0
+        return self.value.float() * gradwrtoutput
 
 
+class LeakyReLU(Module):
+    def __init__(self ):
+        super().__init__()
+        self.name = 'LeakyReLU'
+        self.value = 0
+    
+    def forward(self, input):
+        # LeakyReLU(x) = x > 0 = x , x < 0 = a*x
+        self.value = input
+        y = 0.01 * (input < 0).float() * input + (input >= 0).float() * input
+        return y
+    
+    def backward(self, gradwrtoutput):
+        # LeakyReLU(x) = (x > 0 = 1 , x < 0 = a) * gradwrtoutput
+        y = 0.01 * (self.value < 0).float() * gradwrtoutput + (self.value >= 0).float() * gradwrtoutput
+        return y
+    
